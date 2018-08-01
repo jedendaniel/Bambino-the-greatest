643e91547305fa2c8d09f7098f3d4e7884356e65
Dd <jedendaniel@gmail.com>
2018-08-01 11:52:45 +0200

    Add player movement

--- Bambino the greatest/Assets/Scripts/Player.cs
+++ Bambino the greatest/Assets/Scripts/Player.cs
@@ -1,16 +1,89 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    Rigidbody2D myRigidBody;
+
+    public float jumpYVelocity = 300;
+    float actuallJumpHeight = 0;
+    public float jumpRatioPerFrame = 1;
+    public float maxJumpHeight = 20;
+
+    public float groundMovementVelocity;
+    public float airMovementVelocity;
+
+    float actuallMovementVelocity = 0;
+    public float movementRatioPerFrame;
+    public float maxMovementVelocity;
+
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Z))
+        {
+            Jump();
+        }
+        if (Input.GetKey(KeyCode.R))
+        {
+            Reset();
+        }
+        Move(Input.GetAxisRaw("Horizontal"));
+    }
+
+    private void LateUpdate()
+    {
+    }
+
+    private void Move(float input)
+    {
+        float movement = 0;
+        if (input != 0)
+        {
+            float actuallVelocityMagnitude = Math.Abs(myRigidBody.velocity.x);
+            if (actuallVelocityMagnitude < maxMovementVelocity ||
+                (!IsGrounded() && Math.Sign(myRigidBody.velocity.x) != Math.Sign(input)))
+            {
+                float movementPowerDivider = actuallVelocityMagnitude < 1 ? 1f : actuallVelocityMagnitude / 3;
+                movement = input * groundMovementVelocity / movementPowerDivider;
+                myRigidBody.AddForce(new Vector2(movement, 0));
+            }
+        }
+        else
+        {
+            if (IsGrounded()) myRigidBody.velocity = new Vector2(0, myRigidBody.velocity.y);
+        }
+
+    }
+
+    private void Jump()
+    {
+        if (actuallJumpHeight < maxJumpHeight)
+        {
+            actuallJumpHeight += jumpRatioPerFrame;
+            myRigidBody.AddForce(new Vector2(0, jumpYVelocity / actuallJumpHeight));
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        if (actuallJumpHeight > 0) return false;
+        else return true;
+    }
+
+    private void Reset()
+    {
+        actuallJumpHeight = 0;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+    }
 }
